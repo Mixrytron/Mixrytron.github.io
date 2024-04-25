6183aad2054669eea9df0dd4ff0fdc4c4d960225
Mixrytron <91456583+Mixrytron@users.noreply.github.com>
2024-04-25 13:53:49 +0800

    GameDealTracker

--- script.js
+++ script.js
@@ -1,127 +1,123 @@
-particlesJS("particles-js", {
-  particles: {
-    number: {
-      value: 150,
-      density: {
-        enable: true,
-        value_area: 631.3280775270874
-      }
-    },
-    color: {
-      value: "#fff"
-    },
-    shape: {
-      type: "circle",
-      stroke: {
-        width: 0,
-        color: "#000000"
-      },
-      polygon: {
-        nb_sides: 5
-      },
-      image: {
-        src: "img/github.svg",
-        width: 100,
-        height: 100
-      }
-    },
-    opacity: {
-      value: 0.5,
-      random: true,
-      anim: {
-        enable: false,
-        speed: 1,
-        opacity_min: 0.1,
-        sync: false
-      }
-    },
-    size: {
-      value: 5,
-      random: true,
-      anim: {
-        enable: false,
-        speed: 40,
-        size_min: 0.1,
-        sync: false
-      }
-    },
-    line_linked: {
-      enable: false,
-      distance: 500,
-      color: "#ffffff",
-      opacity: 0.4,
-      width: 2
-    },
-    move: {
-      enable: true,
-      speed: 1.5,
-      direction: "bottom",
-      random: false,
-      straight: false,
-      out_mode: "out",
-      bounce: false,
-      attract: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 1200
-      }
+document.addEventListener('DOMContentLoaded', () => {
+    const wishlist = JSON.parse(localStorage.getItem('wishlist'));
+    if (wishlist.length > 0) {
+        loadWishlist();
+    } else {
+        loadBrowse();
     }
-  },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: {
-        enable: false,
-        mode: "bubble"
-      },
-      onclick: {
-        enable: true,
-        mode: "repulse"
-      },
-      resize: true
-    },
-    modes: {
-      grab: {
-        distance: 400,
-        line_linked: {
-          opacity: 0.5
-        }
-      },
-      bubble: {
-        distance: 400,
-        size: 4,
-        duration: 0.3,
-        opacity: 1,
-        speed: 3
-      },
-      repulse: {
-        distance: 200,
-        duration: 0.4
-      },
-      push: {
-        particles_nb: 4
-      },
-      remove: {
-        particles_nb: 2
-      }
-    }
-  },
-  retina_detect: true
 });
-var count_particles, stats, update;
-stats = new Stats();
-stats.setMode(0);
-stats.domElement.style.position = "absolute";
-stats.domElement.style.left = "0px";
-stats.domElement.style.top = "0px";
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector(".js-count-particles");
-update = function() {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(update);
-};
-requestAnimationFrame(update);
+
+const loadWishlist = () => {
+    const card = document.getElementById('card');
+    card.innerHTML = `
+        <p id="wishlistTitle">Your Wishlist</p>
+        <div id="dealsContainer"></div>
+        <button id="refreshButton" onclick="loadWishlist()">⟳</button>
+    `;
+    const wishlist = JSON.parse(localStorage.getItem('wishlist'));
+    if (wishlist.length > 0) {
+        getDeals(wishlist);
+    }
+}
+
+const loadBrowse = () => {
+    const card = document.getElementById('card');
+    card.innerHTML = `
+        <div id="search">
+            <input type="text" placeholder="Enter game title..." id="searchInput">
+            <button onclick="searchGames()">⌕</button>
+        </div>
+        <div id="dealsContainer"></div>     
+    `;
+}
+
+const searchGames = async () => {
+    var searchQuery = document.getElementById('searchInput').value;
+    var title = searchQuery.replaceAll(' ','_');
+
+    try {
+        const response = await fetch('https://www.cheapshark.com/api/1.0/games?title='+title);
+        if (response.ok) {
+            const gameList = await response.json();
+            let gameIDs = '';
+            for (game in gameList) {
+                gameIDs += gameList[game].gameID + ',';
+            }
+            gameIDs = gameIDs.slice(0, -1); 
+            getDeals(gameIDs);
+        } else {
+            console.error('Error:', response.status);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const getDeals = async (gameIDs) => {
+    try {
+        const response = await fetch('https://www.cheapshark.com/api/1.0/games?ids='+gameIDs)
+        if (response.ok) {
+            const dealList = await response.json();
+            displayDeals(dealList);
+        } else {
+            console.error('Error:', response.status);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const displayDeals = (dealList) => {
+    const container = document.getElementById('dealsContainer'); 
+    container.innerHTML = ''; 
+
+    const gamesArray = Object.values(dealList);
+    const idArray = Object.keys(dealList);
+    const wishlist = JSON.parse(localStorage.getItem('wishlist')) || [];
+
+    gamesArray.forEach((game, index) => {
+        const div = document.createElement('div');
+        let bestDeal = game.deals[0];
+        let gameID = idArray[index];
+
+        div.className = 'game-deal';
+        div.innerHTML = `
+            <a href="https://www.cheapshark.com/redirect?dealID=${bestDeal.dealID}" target="_blank" rel="noopener noreferrer">
+                <img src="${game.info.thumb}" class='thumb' alt="${game.info.title}">
+                <h3 class='title'>${game.info.title}</h3>
+                <div class='price'>
+                    <p>$${bestDeal.price}</p>
+                    <p>-${100-Math.round(bestDeal.price/bestDeal.retailPrice*100)}%</p>
+                    <p style="text-decoration: line-through;">$${bestDeal.retailPrice}</p>
+                </div>
+            </a>
+        `;
+
+        const button = document.createElement('button');
+        button.textContent = wishlist.includes(gameID) ? '-' : '+';
+        button.style.backgroundColor = wishlist.includes(gameID) ? 'red' : 'limegreen';
+        button.addEventListener('click', () => {
+            toggleWishlist(gameID, button);
+        });
+        div.appendChild(button);
+
+        container.appendChild(div); 
+    });
+}
+
+const toggleWishlist = (gameID, button) => {
+    const wishlist = JSON.parse(localStorage.getItem('wishlist'));
+    if (wishlist.includes(gameID)) {
+        wishlist.splice(wishlist.indexOf(gameID), 1);
+        button.textContent = '+';
+        button.style.backgroundColor = 'limegreen';
+    } else {
+        wishlist.push(gameID);
+        button.textContent = '-';
+        button.style.backgroundColor = 'red';
+    }
+    localStorage.setItem('wishlist', JSON.stringify(wishlist));
+}
+
+
+
